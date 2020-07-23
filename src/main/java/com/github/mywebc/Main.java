d38be1dc99c0ef5710d6df0da2cf9af6b622f62a
陈晓拉尼 <2524729719@qq.com>
2020-07-23 11:30:15 +0800

    Basic algorithm 重构代码 (#2)
    * 初步爬取
    
    * 重构代码
    
    * 去除阿里云镜像

--- src/main/java/com/github/mywebc/Main.java
+++ src/main/java/com/github/mywebc/Main.java
@@ -6,19 +6,91 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        // 待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        // 已经处理的链接池子
+        Set<String> processLinks = new HashSet<>();
+
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            // ArrayList 从尾部删除更有效率
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processLinks.contains(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a").stream().map(aTag -> aTag.attr("attr")).forEach(linkPool::add);
+
+                storeInToDataBaseIfItIsNewsPage(doc);
+                processLinks.add(link);
+            } else {
+                continue;
+            }
+        }
+    }
+
+    private static void storeInToDataBaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        // 是否是我们感兴趣的
         CloseableHttpClient httpclient = HttpClients.createDefault();
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        System.out.println(link);
         HttpGet httpGet = new HttpGet("https://sina.cn");
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4181.9 Safari/537.36");
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity1));
+
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
 
         }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLogin(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLogin(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
